af91dcadc5300f279d6643bdcd7fa0caf26ff275
Mykola Kizan <mkizan@gmail.com>
2023-11-27 12:53:41 +0200

    fix some files for build

--- src/components/ReferenceDirectoriesListItem/ReferenceDirectoriesListItem.stories.ts
+++ src/components/ReferenceDirectoriesListItem/ReferenceDirectoriesListItem.stories.ts
@@ -1,12 +1,14 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta } from "@storybook/react";
 import ReferenceDirectoriesListItem from "./ReferenceDirectoriesListItem";
 
+// eslint-disable-next-line storybook/story-exports
 const meta: Meta<typeof ReferenceDirectoriesListItem> = {
-  title: "ReferenceDirectoriesListItem",
+  title: "./ReferenceDirectoriesListItem.tsx",
   component: ReferenceDirectoriesListItem,
 };
 
 export default meta;
-type Story = StoryObj<typeof ReferenceDirectoriesListItem>;
+// type Story = StoryObj<typeof ReferenceDirectoriesListItem>;
+// export const Primary: Story = () => <ReferenceDirectoriesListItem />
 
 // const Template = (args) => <ReferenceDirectoriesListItem {...args} />;
