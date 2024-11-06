81c7a038a540de331f6446d7060170ac2b701b18
Mykola Kizan <mkizan@gmail.com>
2024-11-06 06:30:15 +0200

    delete ReferenceDirectories component

--- src/api/referenceDirectoriesAPI.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:3004";
-
-export const getReferenceDirectoriesList = async () => {
-  const response = await axios.get("/referenceDirectoriesList");
-  return response.data;
-};
--- src/redux/referenceDirectoriesList/operations.ts
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getReferenceDirectoriesList } from "../../api/referenceDirectoriesAPI";
-
-export const referenceDirectoriesListThunk = createAsyncThunk(
-  "referenceDirectoriesList/get",
-  async () => await getReferenceDirectoriesList()
-);
