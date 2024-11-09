0e28f657fa58041ad48275d752fc739ce19fdf70
Mykola Kizan <mkizan@gmail.com>
2024-11-09 06:51:23 +0200

    add tranlates

--- src/components/OrganizationsList/OrganizationsList.tsx
+++ src/components/OrganizationsList/OrganizationsList.tsx
@@ -1,8 +1,10 @@
 import OrganizationsListItem from "../OrganizationsListItem";
 import { Link } from "react-router-dom";
 import { useGetOrganizationsQuery } from "../../redux/api/organiationsApi";
+import { useTranslation } from "react-i18next";
 
 const OrganizationsList = () => {
+  const { t } = useTranslation();
   const {
     data: organizations,
     isLoading,
@@ -11,16 +13,17 @@ const OrganizationsList = () => {
 
   return (
     <>
-      {isError && <div>Error while loading organizations</div>}
-      {isLoading && <div>Loading...</div>}
+      {isError && <div>{t("ns:error.message.loading")}</div>}
+      {isLoading && <div>{t("ns:text.status.loading")}</div>}
       <button type="button" className="px-3 py-1 font-bold border">
         <Link to="/">Back</Link>
       </button>
+      <h3 className="text-center">{t("ns:text.title.organizations")}</h3>
       <table className="table-auto">
         <thead>
           <tr>
-            <th>Префікс</th>
-            <th>Назва організації</th>
+            <th>{t("ns:text.columnTitle.org.prefix")}</th>
+            <th>{t("ns:text.columnTitle.org.name")}</th>
           </tr>
         </thead>
         <tbody>
